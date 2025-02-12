014b287159cabe880ce93ed674fff23cf0ddd2b0
developer_king <renafernandesjorge11@gmail.com>
2025-02-12 17:16:08 +0000

    Atualizar o config.js

--- src/config/config.js
+++ src/config/config.js
@@ -10,7 +10,7 @@ export const config = {
     footer: [
         {
             name: {zh: "支持", en: "Support"},
-            url: "https://github.com/SonMooSans"
+            url: "https://github.com/developerking-s/"
         }
     ],
     settings: detail => [
@@ -27,8 +27,8 @@ export const config = {
     features: {
         "auto_kill_kane": KillKane
     },
-    tutorialUrl: "https://github.com/SonMooSans/discord-bot-dashboard",
-    serverUrl: "http://localhost:8080",
+    tutorialUrl: "https://github.com/developerking-s/dashbord-sambot",
+    serverUrl: "https://",
     inviteUrl: "https://discord.com/api/oauth2/authorize?client_id=1212115074735415316&permissions=8&scope=bot",
     data: {
         dashboard: dashboardData,
